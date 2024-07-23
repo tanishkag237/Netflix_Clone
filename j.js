ed01334d9c5b0fa482836d201a442ef4bfbe102b
tanishkag237 <159025158+tanishkag237@users.noreply.github.com>
2024-07-23 11:28:06 +0530

    Add files via upload

---
+++ j.js
@@ -0,0 +1,34 @@
+// const labels = document.querySelectorAll('.questions li label');
+//     labels.forEach(label => {
+//         label.addEventListener('click', function () {
+//             const isChecked = this.previousElementSibling.checked;
+//             if (isChecked) {
+//                 // If radio is already checked, uncheck it
+//                 this.previousElementSibling.checked = false;
+//             }
+//         });
+//     // });
+
+
+    // Get all question elements
+   // const questions = document.querySelectorAll('.faq .questions li');
+
+    // Loop through each question element
+    // questions.forEach(question => {
+    //     // Add click event listener to each question
+    //     question.addEventListener('click', () => {
+    //         // Toggle the 'active' class on the question
+    //         question.classList.toggle('active');
+
+    //         // Toggle the visibility of the answer
+    //         const answer = question.querySelector('.ans');
+    //         if (answer.style.maxHeight) {
+    //             // If answer is currently visible, hide it
+    //             answer.style.maxHeight = null;
+    //         } else {
+    //             // If answer is currently hidden, show it
+    //             answer.style.maxHeight = answer.scrollHeight + 'px';
+    //         }
+    //     });
+    // });
+
